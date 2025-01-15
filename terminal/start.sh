26cc56629c860151a47c367940df6206e28e6588
Kalpita Mandal <kalpita1703@gmail.com>
2025-01-15 23:50:56 +0530

    Update start script

--- terminal/start.sh
+++ terminal/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
 echo "Start chatting with chromia AI!"
 docker exec -it chromia-neural-interface_chr-node_1 sh -c 'bun run dev'
