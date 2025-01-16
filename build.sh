067cbfbf0fa0f6c4e784f6ddb896b299784731dc
Kalpita Mandal <kalpita1703@gmail.com>
2025-01-16 23:46:12 +0530

    Changes in docker-compose flow

--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@ docker rm -f $(docker ps -a -q)
 docker rmi -f $(docker images -a -q)
 docker system prune -f
 
+# load docker images
+docker load < ./ui/chromia-frontend.tar
+docker load < ./chromia-node.tar
+
 # build using the docker-compose file
 docker-compose up -d 
 
-sleep 40
-# docker exec -it chromia-neural-interface_chr-node_1 sh -c 'bun run dev'
-bun run dev
+# sleep 40
+# # docker exec -it chromia-neural-interface_chr-node_1 sh -c 'bun run dev'
+# bun run dev
